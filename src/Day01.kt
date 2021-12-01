6be7cec06fe68c9f66e4ef1547d970de514c0ad8
Pavel Jurca <pavel.jurca@seznam.cz>
2021-12-01 23:00:25 +0100

    refactor day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,19 +1,12 @@
 fun main() {
-    fun part1(input: List<Int>): Int {
-        return input.zipWithNext { a, b -> b > a }.count { it }
-    }
+    fun part1(input: List<Int>): Int = input
+        .zipWithNext { a, b -> b > a }
+        .count { it }
 
-    fun part2(input: List<Int>): Int {
-        val threeSumsHaha = mutableListOf<Int>()
-
-        for ((index, value) in input.withIndex()) {
-            if (index + 2 <= input.lastIndex) {
-                threeSumsHaha.add(value + input[index + 1] + input[index + 2])
-            }
-        }
-
-        return threeSumsHaha.zipWithNext { a, b -> b > a }.count { it }
-    }
+    fun part2(input: List<Int>): Int = input
+        .windowed(3) { it.sum() }
+        .zipWithNext { a, b -> b > a }
+        .count { it }
 
     val input = readInput("Day01").map { it.toInt() }
     println(part1(input))
