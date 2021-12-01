2904e39c9eb915289458ad1a2782b2c29fce3d1b
Pavel Jurca <pavel.jurca@seznam.cz>
2021-12-01 23:03:03 +0100

    add example check

--- src/Day01.kt
+++ src/Day01.kt
@@ -8,6 +8,10 @@ fun main() {
         .zipWithNext { a, b -> b > a }
         .count { it }
 
+    val exampleInput = readInput("Day01Example").map { it.toInt() }
+    check(part1(exampleInput) == 7)
+    check(part2(exampleInput) == 5)
+
     val input = readInput("Day01").map { it.toInt() }
     println(part1(input))
     println(part2(input))
